764834d27b337bcb191dde73374d757aa6cfac9c
Ridiculous <jokeforce@gmail.com>
2017-01-17 09:36:25 +0800

    完成自动更新漫画内容的计划任务，每天定时执行

--- app/tasks.py
+++ app/tasks.py
@@ -1,17 +1,83 @@
 #-*-coding:utf-8-*-
-from app import db
 import requests
-from flask import jsonify
-#更新漫画到七牛云
+import re
+from bs4 import BeautifulSoup
+from app.utils.ftp import MyFTP
+
 def comic_job():
-    #从数据库获取所有漫画信息
-    url = 'http://127.0.0.1:5000/api/v1/comics'
-    r = requests.get(url)
+    #get all comics from database
+    base_url = 'http://127.0.0.1:5000/api/v1/comics'
+    r = requests.get(base_url)
     json_result = r.json()
+
+    #ftp init
+    myftp = MyFTP('202.102.41.224', 21, 'admin', 'admin123', '.')
+    myftp.login()
+
+    #log in
+    login_url = 'http://www.icartoons.cn/index.php?m=member&c=index&a=login'
+    s = requests.Session()
+    payload = {
+        'username': '18118999630',
+        'password': '1q2w3e4r',
+        'dosubmit': '1'
+    }
+    r = s.post(login_url, data=payload)
+    #
     if json_result['code'] == '0':
         data = json_result['data']
         for item in data:
             id = item['comicid']
+            name = item['comicname']
+            local_state = item['state']
+            local_chapter = item['curchapter']
+            remote_state = '0'
+            remote_chapter = 0
 
+            print 'update %s...' % name
+            if local_state == '1':
+                continue
+            else:
+                # requests
+                request_url = 'http://www.icartoons.cn/index.php?m=content&c=index&a=show&catid=25&id=%s' % id
+                r = s.get(request_url)
+                # check state
+                match = re.search(u'状态：(&nbsp;)+(.*)<', r.text)
+                if match:
+                    state = match.group(2)
+                    if state == u'已完结':
+                        remote_state = '1'
+                #parse
+                soup = BeautifulSoup(r.text, 'html.parser')
+                for a_tag in soup.find_all('a'):
+                    content = a_tag.get_text()
+                    if re.search(u'第\d+集', content):
+                        episode = re.search(u'第(\d+)集', content).group(1)
+                        if int(episode) > remote_chapter:
+                            remote_chapter = int(episode)
+                        #update episode
+                        if int(episode) > local_chapter:
+                            print 'add chapter %s' % episode
+                            provisionid = re.search(u'provisionid=(\d+)', unicode(a_tag)).group(1)
+                            img_url = 'http://www.icartoons.cn/index.php?m=content&c=index&a=play_comic&id=%s&provisionid=%s' % (
+                                id, provisionid)
+                            r = s.get(img_url)
+                            img_soup = BeautifulSoup(r.text, 'html.parser')
+                            for img_tag in img_soup.find_all('img'):
+                                if img_tag.has_attr('id'):
+                                    print img_tag['id'], img_tag['src']
+                                    if len(img_tag['src']) < 30:
+                                        continue
+                                    myftp.uploadFiles(img_tag['src'], 'comic/' + id + '/' + episode,
+                                                      img_tag['id'][3:] + '.jpg')
+            #update database
+            if remote_chapter > local_chapter:
+                url = base_url + '/' +id
+                payload = {
+                    'state':remote_state,
+                    'curchapter':remote_chapter
+                }
+                r = requests.put(url, data=payload)
+                print r.text
 
 comic_job()
--- tests/api_comic_test.py
+++ tests/api_comic_test.py
@@ -1,3 +1,4 @@
+#-*-coding:utf-8-*-
 import requests
 
 base_url = 'http://127.0.0.1:5000/api/v1/comics'
@@ -6,16 +7,16 @@ base_url = 'http://127.0.0.1:5000/api/v1/comics'
 def add_test():
     url = base_url
     payload = {
-        'comicid':'1002',
-        'comicname':'test2',
+        'comicid':'200099420',
+        'comicname':'混沌少女3',
         'packageid':'1',
-        'brief':'induction',
-        'author':'sk',
-        'category':'rexue',
+        'brief':'《混沌少女3》是五个少年无意中开启了一所精神病院的保险柜，解开了混沌少女希希莉亚的封印。从而被混沌神少女选作混沌战士五小强，一幕幕超能力闹剧即将登场',
+        'author':'图：张琨/文：张琨',
+        'category':'神魔',
         'hits':10000,
-        'state':'1',
+        'state':'0',
         'cover':'www.baidu.com',
-        'curchapter':100,
+        'curchapter':0,
         'freechapter':20
     }
     r = requests.post(url, data=payload)
