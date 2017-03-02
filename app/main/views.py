dc9153db030fd6e3d0b3979da6b4dfbd80214b56
Nynia <jokeforce@gmail.com>
2017-03-02 16:19:04 +0800

    修复一个小bug

--- app/main/views.py
+++ app/main/views.py
@@ -18,7 +18,7 @@ def index():
     if request.method == 'POST':
         game = Game()
         myftp = MyFTP(config.FTP_ADDR,config.FTP_PORT,config.FTP_USER,config.FTP_PWD, '/')
-        myftp.login()
+        #myftp.login()
         prefix = config.STATIC_URL_PREFIX
         for key, value in form.data.items():
             if hasattr(game, key):
@@ -29,7 +29,7 @@ def index():
             filename = generate_name(filename)
             print filename
             file.save(os.path.join('./res/', filename))
-            myftp.uploadFiles(os.path.join('./res/', filename), '/games/')
+            #myftp.uploadFiles(os.path.join('./res/', filename), '/games/')
             game.img_icon = prefix + 'games/' + filename
         if form.data['type'] == '1':
             file = request.files['apk']
@@ -38,13 +38,13 @@ def index():
                 filename = generate_name(filename)
                 print filename
                 file.save(os.path.join('./res/', filename))
-                myftp.uploadFiles(os.path.join('./res/', filename), '/games/')
+                #myftp.uploadFiles(os.path.join('./res/', filename), '/games/')
                 game.url = prefix + 'games/' + filename
-            for index,filename in enumerate(request.files.getlist('screenshot')):
-                filename = generate_name(secure_filename(filename))
+            for index,file in enumerate(request.files.getlist('screenshot')):
+                filename = generate_name(secure_filename(file.filename))
                 print filename
                 file.save(os.path.join('./res/', filename))
-                myftp.uploadFiles(os.path.join('./res/', filename), '/games/')
+                #myftp.uploadFiles(os.path.join('./res/', filename), '/games/')
                 setattr(game, 'img_screenshot_'+str(index+1), prefix + 'games/' + filename)
         game.createtime = datetime.now().strftime('%Y%m%d%H%M%S')
         db.session.add(game)
