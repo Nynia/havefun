7b9c6029e47fa8f938a876eb086851c39ef7a7b7
Nynia <jokeforce@gmail.com>
2017-03-16 17:13:47 +0800

    111

--- app/main/views.py
+++ app/main/views.py
@@ -107,8 +107,8 @@ def comicbrowse(id):
     chapter = request.args.get('chapter')
     myftp = MyFTP(config.FTP_ADDR, config.FTP_PORT, config.FTP_USER, config.FTP_PWD, '/')
     myftp.login()
-    filelist = myftp.listfiles('/comics'+id)
-    filelist = ['/comics'+id+'/'+item for item in filelist]
+    filelist = myftp.listfiles('/comics'+'/'+id)
+    filelist = ['/comics'+'/'+id+'/'+item for item in filelist]
     print filelist
     return render_template('cartoon_browse.html',imgs=filelist)
 
