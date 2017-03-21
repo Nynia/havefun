07ee127cfd8470a4d8fce7a6aa8eed73fd699d9f
Nynia <jokeforce@gmail.com>
2017-03-21 14:49:18 +0800

    debug

--- app/main/views.py
+++ app/main/views.py
@@ -132,7 +132,7 @@ def readinginfo():
     for c in chapters:
         chaptername = c.chaptername.split(' ')
         dict = {}
-        dict['a'] = chaptername[0].split('-')[0]
+        dict['a'] = chaptername[0].split('-')[1]
         dict['b'] = chaptername[1] if len(chaptername)>1 else u'前言'
         dict['id'] = c.chapterid
         chapter_dict_list.append(dict)
