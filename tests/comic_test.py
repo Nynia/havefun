c41d9a04b8e8b130a48e9938d3bbfc86ca5dd1ce
Nynia <jokeforce@gmail.com>
2017-03-31 09:03:30 +0800

    debug

--- tests/comic_test.py
+++ tests/comic_test.py
@@ -86,7 +86,7 @@ class MyFTP:
                 self.ftp.cwd(dir)
 
 
-def update_comics(id):
+def update_comics(id_list):
     login_url = 'http://www.icartoons.cn/index.php?m=member&c=index&a=login'
     # login
     s = requests.Session()
@@ -96,55 +96,53 @@ def update_comics(id):
         'dosubmit': '1'
     }
     r = s.post(login_url, data=payload)
-    print r.text
-    # requests
-    request_url = 'http://www.icartoons.cn/index.php?m=content&c=index&a=show&catid=25&id=%s' % id
-    r = s.get(request_url)
-    # check state
-    match = re.search(u'状态：(&nbsp;)+(.*)<', r.text)
-    if match:
-        state = match.group(2)
-        if state == u'已完结':
-            print id, state
-            # return
+    #print r.text
     myftp = MyFTP('192.168.114.138', 12345, 'jsgx', 'jsgx2017', '/')
-    # myftp = MyFTP('61.160.185.51', 11145, 'jsgx', 'jsgx2017', '.')
     myftp.login()
-    soup = BeautifulSoup(r.text, 'html.parser')
-    for ss in soup.find_all(id=re.compile("positive_setinfo_*")):
-        for a_tag in ss.find_all('a'):
-            content = a_tag.get_text()
-            if re.search(u'第\d+集', content):
-                episode = re.search(u'第(\d+)集', content).group(1)
-                provisionid = re.search(u'provisionid=(\d+)', unicode(a_tag)).group(1)
-                # print episode, provisionid
-                img_url = 'http://www.icartoons.cn/index.php?m=content&c=index&a=play_comic&id=%s&provisionid=%s' % (
-                    id, provisionid)
-                print img_url
-                r = s.get(img_url)
-                img_soup = BeautifulSoup(r.text, 'html.parser')
-                for img_tag in img_soup.find_all('img'):
-                    if img_tag.has_attr('id'):
-                        print episode, img_tag['id'], img_tag['src']
-                        if len(img_tag['src']) < 30:
-                            continue
-                        target_name = '_'.join((id, episode, img_tag['id'][3:])) + '.jpg'
-                        target_dir = id + os.sep + episode
-                        # download pics
-                        # myftp.changewd('comic/20082903/123')
-                        myftp.uploadFiles(img_tag['src'], '/comics/' + id + '/' + episode, img_tag['id'][3:] + '.jpg')
-                        # with open(target_path, 'wb') as handle:
-                        #    response = requests.get(img_tag['src'], stream=True)
-                        #    for block in response.iter_content(1024):
-                        #        if not block:
-                        #            break
-                        #        handle.write(block)
-                        # upload_qiniu(target_path, target_name)
 
+    for id in id_list:
+        request_url = 'http://www.icartoons.cn/index.php?m=content&c=index&a=show&catid=25&id=%s' % id
+        r = s.get(request_url)
+        # check state
+        match = re.search(u'状态：(&nbsp;)+(.*)<', r.text)
+        if match:
+            state = match.group(2)
+            if state == u'已完结':
+                print id, state
+                # return
+
+        soup = BeautifulSoup(r.text, 'html.parser')
+        for ss in soup.find_all(id=re.compile("positive_setinfo_*")):
+            for a_tag in ss.find_all('a'):
+                content = a_tag.get_text()
+                if re.search(u'第\d+集', content):
+                    episode = re.search(u'第(\d+)集', content).group(1)
+                    provisionid = re.search(u'provisionid=(\d+)', unicode(a_tag)).group(1)
+                    # print episode, provisionid
+                    img_url = 'http://www.icartoons.cn/index.php?m=content&c=index&a=play_comic&id=%s&provisionid=%s' % (
+                        id, provisionid)
+                    print img_url
+                    r = s.get(img_url)
+                    img_soup = BeautifulSoup(r.text, 'html.parser')
+                    for img_tag in img_soup.find_all('img'):
+                        if img_tag.has_attr('id'):
+                            print episode, img_tag['id'], img_tag['src']
+                            if len(img_tag['src']) < 30:
+                                continue
+                            target_name = '_'.join((id, episode, img_tag['id'][3:])) + '.jpg'
+                            target_dir = id + os.sep + episode
+                            # download pics
+                            # myftp.changewd('comic/20082903/123')
+                            myftp.uploadFiles(img_tag['src'], '/comics/' + id + '/' + episode, img_tag['id'][3:] + '.jpg')
+                            # with open(target_path, 'wb') as handle:
+                            #    response = requests.get(img_tag['src'], stream=True)
+                            #    for block in response.iter_content(1024):
+                            #        if not block:
+                            #            break
+                            #        handle.write(block)
+                            # upload_qiniu(target_path, target_name)
 
 id_list = ['200103359', '200103364', '200103379', '200103383', '200103388', '200103408', '200103424', '200103454',
            '200103457', '200103377', '200103380', '200103382', '200103386', '200103391', '200103393', '200103394',
            '200103396', '200103398', '200103401', '200103403', '200103405']
-#update_comics('200103379')
-for li in id_list:
-    update_comics(li)
+update_comics(id_list)
