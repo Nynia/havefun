173af1efaea23133e564aa38d98710d442cd7d23
Nynia <jokeforce@gmail.com>
2018-03-23 00:42:56 +0800

    debug

--- app/main/views.py
+++ app/main/views.py
@@ -113,6 +113,8 @@ def index():
     game_list.append(Game.query.filter_by(packageid='135000000000000000000').all())
     # for item in RecommendApkGame.query.all():
     #     game_list.append(Game.query.get(int(item.id)))
+    print game_list
+    print len(game_list)
     random.shuffle(game_list)
 
     packages = Package.query.all()
