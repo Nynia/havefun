6f1f7b12a29c4f61a03578de49bf78aeb2928cb4
Nynia <jokeforce@gmail.com>
2018-06-27 10:34:52 +0800

    debug

--- app/api_1_0/tyzone.py
+++ app/api_1_0/tyzone.py
@@ -2,9 +2,10 @@
 from . import api
 from flask import request, jsonify, session
 from app import redis_cli, db
-from urllib import quote
+from urllib import quote_plus
 import hashlib
 from app.models import OrderRelation, OrderHistroy, Package, IntegralStrategy, IntegralRecord, User
+from datetime import datetime
 
 
 @api.route('/tyzone/clientcall', methods=['POST'])
@@ -17,17 +18,14 @@ def clientcall():
     return phonenum
 
 
-@api.route('/tyzone/callback', methods=['POST', 'GET'])
-def func():
+@api.route('/tyzone/callback', methods=['POST'])
+def zonecall():
     apsecret = '2A74D517F5FB0858E0530100007F613C'
     params = []
-    sig2 = ''
     print request.form.to_dict()
     for key, value in request.form.to_dict().items():
-        if key != 'sig2':
+        if not key.startswith('sig'):
             params.append((key, value))
-        else:
-            sig2 = value
     sorted_params = sorted(params)
     print sorted_params
     key_str = ''
@@ -36,87 +34,110 @@ def func():
             key_str += params[0]
             key_str += params[1]
     print key_str
-
     orderid = request.form.get('txId')
     paytime = request.form.get('payTime')
     type = request.form.get('chargeType')
     chargeid = request.form.get('chargeId')
     result = request.form.get('chargeResult')
+    md5fromnum = request.form.get('md5fromnum')
     print orderid, paytime, type, result, chargeid
-    phonenum = redis_cli.get(orderid)
-    print phonenum
-    # key_quoted = quote(key_str)
-    # print key_quoted
-    # print sig2, hashlib.sha1(key_quoted + apsecret).hexdigest()
-    # if sig2 == hashlib.sha1(key_quoted + apsecret).hexdigest():
-    #     return jsonify({
-    #         'resultCode': '0000',
-    #         'resultDesc': 'accepted'
-    #     })
-    # else:
-    #     return jsonify({
-    #         'resultCode': '0001',
-    #         'resultDesc': 'accept error'
-    #     })
-    # orderrelation = OrderRelation.query.filter_by(phonenum=phonenum).filter_by(productid=productid).first()
-    # if not orderrelation:
-    #     orderrelation = OrderRelation()
-    #
-    # orderhistory = OrderHistroy()
-    # orderaction.productid = productid
-    # orderaction.phonenum = phonenum
-    # orderhistory.productid = productid
-    # orderhistory.phonenum = phonenum
-    # orderrelation.ordertime = timestamp
-    # orderrelation.status = '1'
-    #
-    # orderhistory.action = '1'
-    # orderhistory.createtime = timestamp
-    #
-    # user = User.query.filter_by(phonenum=phonenum).first()
-    # des = ''
-    # record = IntegralRecord.query.filter_by(uid=user.id).filter_by(action=9).first()
-    # if not record:
-    #     des = u'首次订购付费包'
-    # else:
-    #     des = u'订购付费包'
-    #
-    # # integral
-    # integral_strategy = IntegralStrategy.query.filter_by(description=des).first()
-    # if integral_strategy:
-    #     integral = integral_strategy.value
-    #     user = User.query.filter_by(phonenum=phonenum).first()
-    #     if integral_strategy.id == 7:
-    #         user.integral = user.integral + integral_strategy.value
-    #         integral_record = IntegralRecord()
-    #         integral_record.uid = user.id
-    #         integral_record.action = integral_strategy.id
-    #         integral_record.change = integral_strategy.value
-    #         integral_record.timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    #         db.session.add(user)
-    #         db.session.add(integral_record)
-    #     else:
-    #         today = datetime.now().strftime('%Y%m%d')
-    #         record = IntegralRecord.query.filter(timestamp.startswith(today)).filter_by(action=8).all()
-    #         history_integral_today = 0
-    #         if record:
-    #             history_integral_today = 400 + reduce(lambda x, y: x + y, [r.change for r in record])
-    #             print history_integral_today
-    #         if history_integral_today < 600:
-    #             user.integral = user.integral + integral_strategy.value
-    #             integral_record = IntegralRecord()
-    #             integral_record.uid = user.id
-    #             integral_record.action = integral_strategy.id
-    #             integral_record.change = integral_strategy.value
-    #             integral_record.timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    #             db.session.add(user)
-    #             db.session.add(integral_record)
-    # db.session.add(orderrelation)
-    # db.session.add(orderhistory)
-    # db.session.commit()
-    #
-    # session[productid] = 1
-    return jsonify({
-        'resultCode': '0000',
-        'resultDesc': 'accepted'
-    })
+    package = Package.query.filter_by(chargeid=chargeid).first()
+    if package:
+        productid = package.productid
+        phonenum = redis_cli.get(orderid)
+        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+        if result == '-99':
+            # 订购成功
+            orderhistory = OrderHistroy()
+            orderhistory.productid = productid
+            orderhistory.phonenum = phonenum
+            orderhistory.action = '1'
+            orderhistory.createtime = timestamp
+            if not phonenum or md5fromnum != hashlib.md5(phonenum).hexdigest().upper():
+                print 'phonenum md5 not matched'
+                # 只记录订购历史
+            else:
+                # 更新订购关系及积分
+                orderrelation = OrderRelation.query.filter_by(phonenum=phonenum).filter_by(productid=productid).first()
+                if not orderrelation:
+                    orderrelation = OrderRelation()
+                orderrelation.ordertime = timestamp
+                orderrelation.status = '1'
+
+                user = User.query.filter_by(phonenum=phonenum).first()
+                record = IntegralRecord.query.filter_by(uid=user.id).filter_by(action=9).first()
+                if not record:
+                    des = u'首次订购付费包'
+                else:
+                    des = u'订购付费包'
+                # integral
+                integral_strategy = IntegralStrategy.query.filter_by(description=des).first()
+                if integral_strategy:
+                    # integral = integral_strategy.value
+                    user = User.query.filter_by(phonenum=phonenum).first()
+                    if integral_strategy.id == 7:
+                        user.integral = user.integral + integral_strategy.value
+                        integral_record = IntegralRecord()
+                        integral_record.uid = user.id
+                        integral_record.action = integral_strategy.id
+                        integral_record.change = integral_strategy.value
+                        integral_record.timestamp = timestamp
+                        db.session.add(user)
+                        db.session.add(integral_record)
+                    else:
+                        today = datetime.now().strftime('%Y%m%d')
+                        record = IntegralRecord.query.filter(timestamp.startswith(today)).filter_by(action=8).all()
+                        history_integral_today = 0
+                        if record:
+                            history_integral_today = 400 + reduce(lambda x, y: x + y, [r.change for r in record])
+                            print history_integral_today
+                        if history_integral_today < 600:
+                            user.integral = user.integral + integral_strategy.value
+                            integral_record = IntegralRecord()
+                            integral_record.uid = user.id
+                            integral_record.action = integral_strategy.id
+                            integral_record.change = integral_strategy.value
+                            integral_record.timestamp = timestamp
+                            db.session.add(user)
+                            db.session.add(integral_record)
+                db.session.add(orderrelation)
+                db.session.add(orderhistory)
+                db.session.commit()
+        elif result == '-100':
+            # 退订成功
+            orderhistory = OrderHistroy()
+            orderhistory.productid = productid
+            orderhistory.phonenum = phonenum
+            orderhistory.action = '0'
+            orderhistory.createtime = timestamp
+            if not phonenum or md5fromnum != hashlib.md5(phonenum).hexdigest().upper():
+                print 'phonenum md5 not matched'
+                # 只记录订购历史
+            else:
+                # 更新订购关系
+                orderrelation = OrderRelation.query.filter_by(phonenum=phonenum).filter_by(productid=productid).first()
+                if not orderrelation:
+                    orderrelation = OrderRelation()
+                orderrelation.canceltime = timestamp
+                orderrelation.status = '2'
+
+                db.session.add(orderrelation)
+                db.session.add(orderhistory)
+                db.session.commit()
+    else:
+        print 'error: package not found, chargeid: %s' % chargeid
+
+    key_quoted = quote_plus(key_str.encode('utf8'))
+    print key_quoted
+    sig2 = request.form.get('sig2')
+    print sig2, hashlib.sha1(key_quoted + apsecret).hexdigest()
+    if sig2 == hashlib.sha1(key_quoted + apsecret).hexdigest():
+        return jsonify({
+            'resultCode': '0000',
+            'resultDesc': 'accepted'
+        })
+    else:
+        return jsonify({
+            'resultCode': '0001',
+            'resultDesc': 'accept error'
+        })
