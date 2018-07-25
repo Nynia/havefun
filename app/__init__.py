66b8d9c3918f1610a3fe0b556ac19b5fc8c34d8f
Nynia <jokeforce@gmail.com>
2018-07-25 18:12:02 +0800

    debug

--- app/__init__.py
+++ app/__init__.py
@@ -65,7 +65,7 @@ def create_app(config_name):
     logger.addHandler(console_handler)
 
     # 指定日志的最低输出级别，默认为WARN级别
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
 
     from .api_1_0 import api as api_1_0_blueprint
     app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
